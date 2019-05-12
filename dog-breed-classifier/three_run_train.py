ef5535101c0355395df071803e82039320f6dfe0
Peter Siemen <peter.siemen@googlemail.com>
2019-05-12 16:56:01 +0200

    cleanup

--- dog-breed-classifier/three_run_train.py
+++
@@ -1,9 +0,0 @@
-from three_train import  *
-
-
-# train the model
-model_scratch = train(100, loaders_scratch, model_scratch, optimizer_scratch,
-                      criterion_scratch, use_cuda, 'model_scratch.pt')
-
-# load the model that got the best validation accuracy
-model_scratch.load_state_dict(torch.load('model_scratch.pt'))
